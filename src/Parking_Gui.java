be3dd909ae8d2b2c224d442d01624c255953be56
Flap jack <stingbear@sbcglobal.net>
2017-04-05 15:22:29 -0700

    more updates, size ,coloe scheme etc

--- src/Parking_Gui.java
+++ src/Parking_Gui.java
@@ -7,6 +7,12 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import java.awt.Font;
+import javax.swing.JTextPane;
+import javax.swing.JMenu;
+import java.awt.List;
+import java.awt.Color;
+import java.awt.SystemColor;
+import java.awt.Label;
 
 public class Parking_Gui extends JFrame {
 
@@ -31,39 +37,77 @@ public class Parking_Gui extends JFrame {
 	/**
 	 * Create the frame.
 	 */
+	
+	
 	public Parking_Gui() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 823, 651);
+		setBounds(100, 100, 1006, 746);
 		contentPane = new JPanel();
+		contentPane.setBackground(SystemColor.textHighlight);
+		contentPane.setForeground(Color.BLACK);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
+		
+		
+		// First Panel for the Welcoming and the scanning -------------------------------------------------------------------
+		
+		
 		JPanel panel = new JPanel();
-		panel.setBounds(12, 13, 363, 578);
+		panel.setForeground(Color.BLACK);
+		panel.setBounds(26, 33, 426, 641);
 		contentPane.add(panel);
 		panel.setLayout(null);
+	
 		
+		// Drop off Button ---------------------------------------------------------------------------------------------------
 		JButton btnDrop = new JButton("Drop");
-		btnDrop.setBounds(24, 514, 97, 25);
+		btnDrop.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		btnDrop.setBackground(Color.WHITE);
+		btnDrop.setBounds(25, 566, 108, 44);
 		panel.add(btnDrop);
 		
-		JButton btnRetrive = new JButton("Retrive");
-		btnRetrive.setBounds(217, 514, 97, 25);
+		
+		
+		
+		//Pick up button -------------------------------------------------------------------------------------------------------
+		JButton btnRetrive = new JButton("Retrieve");
+		btnRetrive.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		btnRetrive.setBackground(Color.WHITE);
+		btnRetrive.setBounds(273, 566, 115, 44);
 		panel.add(btnRetrive);
 		
-		JLabel lblWelcomeToDynamic = new JLabel("Welcome to Dynamic Parking");
-		lblWelcomeToDynamic.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblWelcomeToDynamic.setBounds(57, 27, 257, 25);
+		
+		
+		// Labels for the welcoming screen --------------------------------------------------------------------------------------
+		
+		JLabel lblWelcomeToDynamic = new JLabel("Welcome to High Density ");
+		lblWelcomeToDynamic.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		lblWelcomeToDynamic.setBounds(46, 40, 342, 102);
 		panel.add(lblWelcomeToDynamic);
 		
+		Label label = new Label("Parking ");
+		label.setFont(new Font("Dialog", Font.PLAIN, 30));
+		label.setBounds(135, 143, 124, 44);
+		panel.add(label);
+		
+		
+		
+		
+		
+		
+		
+		
+		// Second Panel for the Tiles Moving----------------------------------------------------------------------------------------- 
 		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(423, 13, 370, 578);
+		panel_1.setBounds(502, 33, 450, 641);
 		contentPane.add(panel_1);
 		panel_1.setLayout(null);
 		
 		JLabel lblxThingsHere = new JLabel("8X8 THINGS HERE");
-		lblxThingsHere.setBounds(133, 76, 147, 16);
+		lblxThingsHere.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblxThingsHere.setBounds(132, 301, 252, 16);
 		panel_1.add(lblxThingsHere);
 	}
 }
