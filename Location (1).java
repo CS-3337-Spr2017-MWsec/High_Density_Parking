31d44b85f432ee3d31353f93964277117ad3cd7d
esokolo <sokolovatennis@yandex.ru>
2017-05-03 19:35:50 -0700

    added location

---
+++ Location (1).java
@@ -0,0 +1,120 @@
+
+public class Location 
+{
+private int x,y;
+
+// empty Constructor
+public Location()
+{
+	
+}
+// base line Constructor
+public Location( int x, int y)
+{
+	this.x = x;
+	this.y = y;
+}
+
+// baseline Getter's and Setters
+public int getX()
+{
+	return x;
+}
+public int getY()
+{
+	return y;
+}
+public void setX(int e)
+{
+	y = e;
+}
+public void setY(int e)
+{
+	x = e;
+}
+
+public boolean equals(Location c)
+{
+	if( this.x == c.x && this .y == c.y)
+	{
+		return true;
+	}
+	return false;
+}
+// basic move functions.. the boundary of which will be determined by PARKINGGRID
+public void moveUP()
+{
+ y--;	
+}
+public void moveRight()
+{
+ x++;	
+}
+public void moveDown()
+{
+ y++;	
+}
+public void moveLeft()
+{
+ x--;	
+}
+
+public String toString()
+{
+	return "("+x+","+y+")";
+}
+public Location UP()
+{
+	return new Location(this.x,this.y-1);
+}
+public Location DOWN()
+{
+	return new Location(this.x,this.y+1);
+}
+public Location RIGHT()
+{
+	return new Location(this.x+1,this.y);
+}
+public Location LEFT()
+{
+	return new Location(this.x-1,this.y);
+}
+
+public boolean toLeft(Location l)
+{
+	boolean r = false;
+	if(this.x < l.x)
+	{
+		r = true;
+	}
+	return r;
+}
+public boolean toRight(Location l)
+{
+	boolean r = false;
+	if(this.x > l.x)
+	{
+		r = true;
+	}
+	return r;
+}
+public boolean above(Location l)
+{
+	boolean r = false;
+	if(this.y < l.y)
+	{
+		r = true;
+	}
+	return r;
+}
+public boolean below(Location l)
+{
+	boolean r = false;
+	if(this.y > l.y)
+	{
+		r = true;
+	}
+	return r;
+}
+
+}
